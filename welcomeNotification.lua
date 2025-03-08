06e1ba12d313c3ce91edef9b76a3784c9cb27f8d
BinbinvoGamingVNN <122621965+BinbinvoGamingVNClone@users.noreply.github.com>
2025-03-08 18:31:03 +0700

    Create welcomeNotification.lua

---
+++ welcomeNotification.lua
@@ -0,0 +1,2 @@
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+NotificationLibrary:SendNotification("Warning", "Chào Mừng Đến Với TDT", 10)
